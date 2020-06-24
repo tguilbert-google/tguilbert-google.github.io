64e6b5d8d6d7fd6e616d63bebae02db4ad7164b2
Thomas Guilbert <tguilbert@chromium.org>
2020-06-24 14:35:20 -0700

    Add preservesPitch WebAudio test

---
+++ preserves_pitch/pitch-detect.js
@@ -0,0 +1,105 @@
+/*
+The MIT License (MIT)
+Copyright (c) 2014 Chris Wilson
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+var audioContext = null;
+
+function getPitchDetector(media) {
+  if (!audioContext)
+    audioContext = new AudioContext();
+
+  var sourceNode = audioContext.createMediaElementSource(media);
+
+  var analyser = audioContext.createAnalyser();
+  analyser.fftSize = 2048;
+
+  sourceNode.connect(analyser);
+  analyser.connect(audioContext.destination);
+
+  return () => { return getPitch(analyser); };
+}
+
+function getPitch(analyser) {
+  analyser.getFloatTimeDomainData( buf );
+  return autoCorrelate( buf, audioContext.sampleRate );
+}
+
+var buflen = 1024;
+var buf = new Float32Array( buflen );
+
+var MIN_SAMPLES = 0;  // will be initialized when AudioContext is created.
+var GOOD_ENOUGH_CORRELATION = 0.9; // this is the "bar" for how close a correlation needs to be
+
+function autoCorrelate( buf, sampleRate ) {
+  var SIZE = buf.length;
+  var MAX_SAMPLES = Math.floor(SIZE/2);
+  var best_offset = -1;
+  var best_correlation = 0;
+  var rms = 0;
+  var foundGoodCorrelation = false;
+  var correlations = new Array(MAX_SAMPLES);
+
+  for (var i=0;i<SIZE;i++) {
+    var val = buf[i];
+    rms += val*val;
+  }
+  rms = Math.sqrt(rms/SIZE);
+  if (rms<0.01) { // not enough signal
+    console.log("Not enough signal");
+    return -1;
+  }
+
+  var lastCorrelation=1;
+  for (var offset = MIN_SAMPLES; offset < MAX_SAMPLES; offset++) {
+    var correlation = 0;
+
+    for (var i=0; i<MAX_SAMPLES; i++) {
+      correlation += Math.abs((buf[i])-(buf[i+offset]));
+    }
+    correlation = 1 - (correlation/MAX_SAMPLES);
+    correlations[offset] = correlation; // store it, for the tweaking we need to do below.
+    if ((correlation>GOOD_ENOUGH_CORRELATION) && (correlation > lastCorrelation)) {
+      foundGoodCorrelation = true;
+      if (correlation > best_correlation) {
+        best_correlation = correlation;
+        best_offset = offset;
+      }
+    } else if (foundGoodCorrelation) {
+      // short-circuit - we found a good correlation, then a bad one, so we'd just be seeing copies from here.
+      // Now we need to tweak the offset - by interpolating between the values to the left and right of the
+      // best offset, and shifting it a bit.  This is complex, and HACKY in this code (happy to take PRs!) -
+      // we need to do a curve fit on correlations[] around best_offset in order to better determine precise
+      // (anti-aliased) offset.
+
+      // we know best_offset >=1,
+      // since foundGoodCorrelation cannot go to true until the second pass (offset=1), and
+      // we can't drop into this clause until the following pass (else if).
+      var shift = (correlations[best_offset+1] - correlations[best_offset-1])/correlations[best_offset];
+      return sampleRate/(best_offset+(8*shift));
+    }
+    lastCorrelation = correlation;
+  }
+  if (best_correlation > 0.01) {
+    // console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")")
+    return sampleRate/best_offset;
+  }
+  return -1;
+}
