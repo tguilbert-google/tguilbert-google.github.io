b400f6375dbb88d2eb99a609751cae3cb93e764f
Thomas Guilbert <tguilbert@chromium.org>
2021-05-26 13:37:24 -0700

    Add test page with 2 decoders

---
+++ webcodecs/two_codecs/test_support/annexb_decoding.any.js
@@ -0,0 +1,85 @@
+// META: global=window,dedicatedworker
+// META: script=/wpt_internal/webcodecs/encoder_utils.js
+
+promise_test(async t => {
+  let outputs = 0;
+  const chunks = [
+    {
+      offset: 0,
+      size: 4045
+    },
+    {
+      offset: 4045,
+      size: 926
+    },
+    {
+      offset: 4971,
+      size: 241
+    },
+    {
+      offset: 5212,
+      size: 97
+    },
+    {
+      offset: 5309,
+      size: 98
+    },
+    {
+      offset: 5407,
+      size: 624
+    },
+    {
+      offset: 6031,
+      size: 185
+    },
+    {
+      offset: 6216,
+      size: 94
+    },
+    {
+      offset: 6310,
+      size: 109
+    },
+    {
+      offset: 6419,
+      size: 281
+    }
+  ];
+
+  let all_chunks_buffer = await (await fetch('annexb.h264')).arrayBuffer();
+  let decoder = new VideoDecoder({
+    error: () => t.unreached_func("Unexpected error"),
+    output: (frame) => { outputs++; frame.close(); },
+  });
+
+  let decoder_2 = new VideoDecoder({
+    error: () => t.unreached_func("Unexpected error"),
+    output: (frame) => { frame.close(); },
+  });
+
+  const config = {
+    codec: "avc1.42001E",
+    codedWidth: 320,
+    codedHeight: 240
+    /* no description means Annex B */
+  };
+  decoder.configure(config);
+  decoder_2.configure(config);
+  let counter = 0;
+  for (let chunk of chunks) {
+    let buffer = new Uint8Array(all_chunks_buffer, chunk.offset, chunk.size);
+    let encoded_chunk = new EncodedVideoChunk({
+      type: (counter == 0) ? 'key' : 'delta',
+      timestamp: counter * 1_00_000,
+      data: buffer
+    });
+    counter++;
+    decoder.decode(encoded_chunk);
+    decoder_2.decode(encoded_chunk);
+  }
+
+  await Promise.all([decoder.flush(), decoder_2.flush()]) ;
+  decoder.close();
+  decoder_2.close();
+  assert_equals(outputs, chunks.length);
+}, "Decode AnnexB");
